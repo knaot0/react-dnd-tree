61ad07fb51f43f7b75f30b06d7b2b16eab0af6b2
knaot0 <naoto5018@gmail.com>
2022-08-17 23:10:08 +0900

    chore: replaceArrayElementsにコメントを追加、変数名をspacerIndexに変更

--- src/utils/replaceArrayElements.ts
+++ src/utils/replaceArrayElements.ts
@@ -2,17 +2,20 @@
  * 配列の要素を入れ替える
  */
 export const replaceArrayElements = <T>(
+  /** 配列 */
   array: T[],
+  /** 移動元のindex */
   sourceIndex: number,
-  targetIndex: number
+  /** 移動先スペーサーのindex */
+  spacerIndex: number
 ): T[] => {
   const newArray = [...array];
 
-  if (targetIndex <= sourceIndex) {
-    newArray.splice(targetIndex, 0, newArray[sourceIndex]);
+  if (spacerIndex <= sourceIndex) {
+    newArray.splice(spacerIndex, 0, newArray[sourceIndex]);
     newArray.splice(sourceIndex + 1, 1);
   } else {
-    newArray.splice(targetIndex, 0, newArray[sourceIndex]);
+    newArray.splice(spacerIndex, 0, newArray[sourceIndex]);
     newArray.splice(sourceIndex, 1);
   }
 
