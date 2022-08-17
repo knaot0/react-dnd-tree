d5b5cde48403831b95c6095813371be02e746ebf
knaot0 <naoto5018@gmail.com>
2022-08-18 00:15:35 +0900

    chore: delete TreeViewDispatchProvider

--- src/providers/TreeViewDispatchProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { TreeViewDispatchContext } from "../contexts/TreeViewDispatchContext";
-
-export const TreeViewDispatchProvider: React.FC = ({ children }) => {
-  return (
-    <TreeViewDispatchContext.Provider value={null}>
-      {children}
-    </TreeViewDispatchContext.Provider>
-  );
-};
