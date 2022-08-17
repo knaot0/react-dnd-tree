b4fe86e373d852fa83d9209251e5f4ced6febbe7
knaot0 <naoto5018@gmail.com>
2022-08-18 00:47:19 +0900

    chore: ライブラリーモードの設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.ts"),
+      fileName: (format) => `my-lib.${format}.js`,
+      name: "MyLib",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+    sourcemap: true,
+  },
+  plugins: [react()],
+});
